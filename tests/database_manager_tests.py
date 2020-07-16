8cd137c07777053b36f9da06b6c897823023b954
Oleg <apramov@mail.ru>
2020-07-16 19:37:38 +0300

    Update tests

--- tests/database_manager_tests.py
+++ tests/database_manager_tests.py
@@ -20,6 +20,8 @@ DROP_TABLE_VAULT = "drop table if exists vault;"
 DROP_TABLE_SHOP = "drop table if exists shop;"
 
 SELECT_SQL = "select * from vault where login= 'user@mail.ru';"
+SELECT_SQL_WITHOUT_NOTE = "select * from vault where\
+    account_id=2;"
 
 class TestExtraMethods(unittest.TestCase):
     """Testing 'database_manager' functions."""
@@ -32,6 +34,8 @@ class TestExtraMethods(unittest.TestCase):
         self.right_insert_dict = {'name': 'mail.ru', 'login': 'user@mail.ru',\
             'password': 'strong password', 'note': 'My note'}
         self.bad_insert_dict = {'name': 'mail.com', 'person': 'Pavel Durov'}
+        self.insert_without_note = {'name': 'mail.ru', 'login': 'user2@mail.ru',\
+            'password': 'hard psswd'}
         connection = sqlite3.connect(self.database_name)
         cursor = connection.cursor()
         cursor.execute(VAULT_TABLE_SQL)
@@ -50,19 +54,32 @@ class TestExtraMethods(unittest.TestCase):
 
     def test_for_database_existing(self):
         """Checking the existence of a database file."""
-        self.assertEqual(True, database_manager.is_database_exists(self.database_name))
-        self.assertEqual(False, database_manager.is_database_exists("tests/somedb.db"))
+        self.assertTrue(database_manager.is_database_exists(self.database_name))
+        self.assertFalse(database_manager.is_database_exists("tests/somedb.db"))
 
     def test_for_table_existing(self):
         """Checking the existence of a table in database.
         Tables 'vault' and 'shop' were created in 'setUp.
         '"""
-        self.assertEqual(True, database_manager.is_table_exists(self.database_name,\
+        self.assertTrue(database_manager.is_table_exists(self.database_name,\
             self.table_name))
-        self.assertEqual(True, database_manager.is_table_exists(self.database_name,\
+        self.assertTrue(database_manager.is_table_exists(self.database_name,\
             self.table_name2))
-        self.assertEqual(False, database_manager.is_table_exists(self.database_name, "big_table"))
-        self.assertEqual(False, database_manager.is_table_exists("tests/somedb.db", "cool_table"))
+        self.assertFalse(database_manager.is_table_exists(self.database_name, "big_table"))
+        self.assertFalse(database_manager.is_table_exists("tests/somedb.db", "cool_table"))
+
+    def test_table_creating(self):
+        """Checking creating of table 'vault'."""
+        connection = sqlite3.connect(self.database_name)
+        cursor = connection.cursor()
+        cursor.execute(DROP_TABLE_VAULT)
+        connection.commit()
+        connection.close()
+        self.assertFalse(database_manager.is_table_exists(self.database_name,\
+            self.table_name))
+        database_manager.create_table_if_not_exists(self.database_name)
+        self.assertTrue(database_manager.is_table_exists(self.database_name,\
+            self.table_name))
 
     def test_for_insert_into_table(self):
         """Checking that insert into table query completed successfully."""
@@ -76,6 +93,13 @@ class TestExtraMethods(unittest.TestCase):
         initial_tuple = (1, ) + tuple(self.right_insert_dict.values())
         # Test it
         self.assertEqual(initial_tuple, right_query_result)
+        # Make right query without note
+        database_manager.insert_into_table(self.database_name, **self.insert_without_note)
+        cursor.execute(SELECT_SQL_WITHOUT_NOTE)
+        query_without_note = cursor.fetchone()
+        initial_tuple2 = (2, ) + tuple(self.insert_without_note.values()) + (None, )
+        # Test it
+        self.assertEqual(initial_tuple2, query_without_note)
         # Make a bad query with KeyError Exception
         bad_query_result = database_manager.insert_into_table(self.database_name,\
             **self.bad_insert_dict)
