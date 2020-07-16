e8af25653d32fda4bd16cd7556593b9c069ae2b3
Oleg <apramov@mail.ru>
2020-07-16 19:13:45 +0300

    Add insert query and test for it

--- src/database_manager.py
+++ src/database_manager.py
@@ -4,10 +4,10 @@
 * make insert, update, searching and remove from database,
 * managment database log file with all manipulations.
 """
+import datetime
+import sqlite3
 import os
 import pytz
-import sqlite3
-import datetime
 
 STORAGE_TABLE_SQL = "create table if not exists vault (\
 account_id integer primary key,\
@@ -16,22 +16,32 @@ login text not null,\
 password text not null,\
 note text);"
 
+LOG_NAME = "log/dbmanager.log"
+
 def is_database_exists(database_name: str) -> bool:
     """Checking for the existence of a database with name 'database_name'."""
-    return os.path.isfile(database_name)
+    result = os.path.isfile(database_name)
+    if not result:
+        database_logger(LOG_NAME, "Warning: Database '{}' doesn't exists".format(database_name))
+    return result
 
 def is_table_exists(database_name: str, table_name: str) -> bool:
-    """Checking for the existence of a table with name 'table_name' in the database with name 'database_name'."""
+    """Checking for the existence of a table with name 'table_name' in the database
+    with name 'database_name'.
+    """
     # If database doesn't exist skip the next operations
     if is_database_exists(database_name):
         connection = sqlite3.connect(database_name)
         cursor = connection.cursor()
-        cursor.execute("select name from sqlite_master where type= 'table' and name='{}';".format(table_name))
+        cursor.execute("select name from sqlite_master where type=\
+            'table' and name='{}';".format(table_name))
         expected_name = cursor.fetchone()
+        connection.close()
         try:
             result = expected_name[0] == table_name
         except TypeError:
             result = False
+            database_logger(LOG_NAME, "Warning: Table '{}' doesn't exists".format(table_name))
     else:
         result = False
     return result
@@ -42,7 +52,36 @@ def create_table_if_not_exists(database_name: str):
     cursor = connection.cursor()
     cursor.execute(STORAGE_TABLE_SQL)
     connection.commit()
-    
+    connection.close()
+    database_logger(LOG_NAME, "Informing: Create table 'vault'")
+
+def insert_into_table(database_name: str, **query_params) -> int:
+    """Insert row with name of record, login, password and notice for record.
+    Where '**query_params' consit of: record, login, password and optional note.
+    """
+    exit_code = 0
+    connection = sqlite3.connect(database_name)
+    cursor = connection.cursor()
+    try:
+        # Where name, login and password arguments from **query_params
+        name = query_params.pop('name')
+        login = query_params.pop('login')
+        password = query_params.pop('password')
+        if 'note' in query_params:
+            note = query_params.pop('note')
+            sql_requst = "insert into vault (name, login, password, note)\
+                values ('{}', '{}', '{}', '{}');".format(name, login, password, note)
+        else:
+            sql_requst = "insert into vault (name, login, password)\
+                values ('{}', '{}', '{}');".format(name, login, password)
+        database_logger(LOG_NAME, "SQL: {}".format(sql_requst))
+        cursor.execute(sql_requst)
+        connection.commit()
+    except KeyError:
+        database_logger(LOG_NAME, "Error: No arguments were passed")
+        exit_code = -1
+    connection.close()
+    return exit_code
 
 def database_logger(file_name: str, message: str):
     """Logging database operations with time of executing operation to log file."""
--- tests/database_manager_tests.py
+++ tests/database_manager_tests.py
@@ -1,5 +1,4 @@
 """Test for 'database_manager' functions."""
-import os
 import sqlite3
 import unittest
 from src import database_manager
@@ -20,6 +19,8 @@ DROP_TABLE_VAULT = "drop table if exists vault;"
 
 DROP_TABLE_SHOP = "drop table if exists shop;"
 
+SELECT_SQL = "select * from vault where login= 'user@mail.ru';"
+
 class TestExtraMethods(unittest.TestCase):
     """Testing 'database_manager' functions."""
 
@@ -28,6 +29,9 @@ class TestExtraMethods(unittest.TestCase):
         self.database_name = "tests/testdb.sqlite3"
         self.table_name = "vault"
         self.table_name2 = "shop"
+        self.right_insert_dict = {'name': 'mail.ru', 'login': 'user@mail.ru',\
+            'password': 'strong password', 'note': 'My note'}
+        self.bad_insert_dict = {'name': 'mail.com', 'person': 'Pavel Durov'}
         connection = sqlite3.connect(self.database_name)
         cursor = connection.cursor()
         cursor.execute(VAULT_TABLE_SQL)
@@ -48,15 +52,36 @@ class TestExtraMethods(unittest.TestCase):
         """Checking the existence of a database file."""
         self.assertEqual(True, database_manager.is_database_exists(self.database_name))
         self.assertEqual(False, database_manager.is_database_exists("tests/somedb.db"))
-    
+
     def test_for_table_existing(self):
         """Checking the existence of a table in database.
         Tables 'vault' and 'shop' were created in 'setUp.
         '"""
-        self.assertEqual(True, database_manager.is_table_exists(self.database_name, self.table_name))
-        self.assertEqual(True, database_manager.is_table_exists(self.database_name, self.table_name2))
+        self.assertEqual(True, database_manager.is_table_exists(self.database_name,\
+            self.table_name))
+        self.assertEqual(True, database_manager.is_table_exists(self.database_name,\
+            self.table_name2))
         self.assertEqual(False, database_manager.is_table_exists(self.database_name, "big_table"))
         self.assertEqual(False, database_manager.is_table_exists("tests/somedb.db", "cool_table"))
+
+    def test_for_insert_into_table(self):
+        """Checking that insert into table query completed successfully."""
+        # Make a right query
+        database_manager.insert_into_table(self.database_name, **self.right_insert_dict)
+        connection = sqlite3.connect(self.database_name)
+        cursor = connection.cursor()
+        cursor.execute(SELECT_SQL)
+        right_query_result = cursor.fetchone()
+        # Prepare initial value to tuple for compare
+        initial_tuple = (1, ) + tuple(self.right_insert_dict.values())
+        # Test it
+        self.assertEqual(initial_tuple, right_query_result)
+        # Make a bad query with KeyError Exception
+        bad_query_result = database_manager.insert_into_table(self.database_name,\
+            **self.bad_insert_dict)
+        self.assertEqual(-1, bad_query_result)
+        connection.close()
+
 
 if __name__ == "__main__":
     unittest.main()
